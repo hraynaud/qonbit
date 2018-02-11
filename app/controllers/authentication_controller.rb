ed11092455f78e89030663d97098a715f4b5dfc9
hraynaud <herby.raynaud@gmail.com>
2018-02-11 13:50:14 -0500

    Completes extraction of twitter and enabling multiple providers

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,14 +1,14 @@
 class AuthenticationController < ApplicationController
 
   def request_token
-    redirect_to oauth_login_by_provider params[:provider]
+    redirect_to Authentication.login_by_provider params[:provider]
   end
 
   def access_token
     request_token = Oauth.find_by(token: params[:oauth_token])
 
     if request_token
-      user_oauth_token = Authentication.produce_user_auth_token(request_token, params[:oauth_verifier])
+      user_oauth_token = Authentication.produce_user_auth_token(request_token, params[:oauth_verifier], params[:provider])
       redirect_to "#{origin}?jwt=#{Authentication.jwt_by_oauth(user_oauth_token)}"
     else
       #TBD ????
@@ -16,14 +16,6 @@ class AuthenticationController < ApplicationController
   end
 
   private
-
-  def oauth_login_by_provider provider
-    case provider
-    when "twitter"
-      Authentication.twitter_login
-    end
-
-  end
 
   def origin
     ENV['ORIGIN']
--- app/services/authentication.rb
+++ app/services/authentication.rb
@@ -1,14 +1,31 @@
 class Authentication
   class << self
 
-    def twitter_login
-      request_token = TWITTER.get_request_token(oauth_callback: TWITTER_OAUTH_CALLBACK)
-      store_token request_token
-      request_token.authorize_url(oauth_callback: TWITTER_OAUTH_CALLBACK)
+    def login_by_provider provider
+      oauth_handler_for(provider).login
     end
 
-    def store_token request_token
-      Oauth.find_or_create_by(token: request_token.token, secret: request_token.secret)
+    def produce_user_auth_token req_token, oauth_verifier, provider
+      acc_token = convert_to_access_token req_token, oauth_verifier, provider
+      token_uid = acc_token.params[:user_id]
+      UserOauthToken.find_by(uid: token_uid) || register_by_oauth(acc_token)
+    end
+
+    def convert_to_access_token oauth, oauth_verifier, provider
+      client = oauth_handler_for(provider).client
+      token = OAuth::RequestToken.new(client, oauth.token, oauth.secret)
+      token.get_access_token(oauth_verifier: oauth_verifier)
+    end
+
+    def jwt_by_oauth token
+      jwt_for token.user_id, token.handle
+    end
+
+    def jwt_for uid, login
+      JWT.encode(
+        { uid: uid, login: login, exp: 1.day.from_now.to_i },
+        Rails.application.secrets.secret_key_base
+      )
     end
 
     def register_by_oauth access_token
@@ -23,32 +40,36 @@ class Authentication
       end
     end
 
-    def convert_to_access_token oauth, oauth_verifier
-      token = OAuth::RequestToken.new(TWITTER, oauth.token, oauth.secret)
-      token.get_access_token(oauth_verifier: oauth_verifier)
-    end
-
-    def produce_user_auth_token req_token, oauth_verifier
-      acc_token = convert_to_access_token req_token, oauth_verifier
-      token_uid = acc_token.params[:user_id]
-      UserOauthToken.find_by(uid: token_uid) || register_by_oauth(acc_token)
-    end
-
-    def jwt_by_oauth token
-      jwt_for token.user_id, token.handle
-    end
-
-    def jwt_for uid, login
-      JWT.encode(
-        { uid: uid, login: login, exp: 1.day.from_now.to_i },
-        Rails.application.secrets.secret_key_base
-      )
+    def oauth_handler_for provider
+      case provider
+      when "twitter"
+        TwitterWrapper
+      end
     end
 
     def login_by_password email, pwd
       #TBD
     end
 
+  end
+
+  class TwitterWrapper
+    class << self
+      def client
+        TWITTER
+      end
+
+      def login
+        request_token = TWITTER.get_request_token(oauth_callback: TWITTER_OAUTH_CALLBACK)
+        store_token request_token, "twitter"
+        request_token.authorize_url(oauth_callback: TWITTER_OAUTH_CALLBACK)
+      end
+
+
+      def store_token request_token, provider
+        Oauth.find_or_create_by(token: request_token.token, secret: request_token.secret, provider: provider)
+      end
+    end
   end
 
 end
