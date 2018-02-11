03cf0249f6b82c6cf7b88d6820b5133292cb4bdb
hraynaud <herby.raynaud@gmail.com>
2018-02-10 22:49:57 -0500

    Removes auth fields from user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210214644) do
+ActiveRecord::Schema.define(version: 20180211025654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,15 +52,10 @@ ActiveRecord::Schema.define(version: 20180210214644) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "uid", null: false
-    t.string "handle", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "email"
     t.string "first_name"
     t.string "last_name"
-    t.index ["uid"], name: "index_users_on_uid"
   end
 
   add_foreign_key "projects", "users"
