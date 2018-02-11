302e94f91021489a8974180cf75a886636dbce65
hraynaud <herby.raynaud@gmail.com>
2018-02-11 11:37:11 -0500

    Code cleanup

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,26 +6,13 @@ class AuthenticationController < ApplicationController
   end
 
   def access_token
-    req_token = Oauth.find_by(token: params[:oauth_token])
+    request_token = Oauth.find_by(token: params[:oauth_token])
 
-    if req_token
-      acc_token = Authentication.convert_to_access_token req_token, params
-      user_oauth_token = UserOauthToken.find_by(uid: acc_token.params[:user_id]) ||
-        Authentication.register_by_auth_token(acc_token)
-
+    if request_token
+      user_oauth_token = Authentication.produce_user_auth_token(request_token, params[:oauth_verifier])
       redirect_to "#{origin}?jwt=#{Authentication.jwt_by_oauth(user_oauth_token)}"
-    else 
-      #TBD ????
-    end
-  end
-
-  def login
-    jwt = Authentication.login_by_password  params[:email], params[:password]
-
-    if jwt
-      pwd_login_success jwt
     else
-      pwd_login_fail
+      #TBD ????
     end
   end
 
