29fe87c0d5e4155d1948fe5041e47b202d26a6d1
hraynaud <herby.raynaud@gmail.com>
2018-02-13 00:12:32 -0500

    spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,14 @@
 class ApplicationController < ActionController::API
 
- def preflight
-   head :ok
- end
+  def preflight
+    head :ok
+  end
 
+  def index
+    render file: 'public/index.html'
+  end
 
- def index
-   render file: 'public/index.html'
- end
-
-
-private
+  private
 
   def authenticate_request
     begin
@@ -21,9 +19,9 @@ private
     end
   end
 
-   def base_client_path
-     "#{origin}?jwt="
-   end
+  def base_client_path
+    "#{origin}?jwt="
+  end
 
   def origin
     ENV['ORIGIN']
