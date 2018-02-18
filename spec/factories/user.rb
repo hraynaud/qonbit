7c377dbb08a81cf020334fb429535d84bb3e3ccd
hraynaud <herby.raynaud@gmail.com>
2018-02-18 18:25:18 -0500

    removes auth fields from user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,12 +1,7 @@
 FactoryBot.define do
 
   factory :user  do |f|
-    f.sequence(:email) { |n| "foo#{n}@example.com" }
     f.sequence(:first_name) { |n| "foo#{n}" }
     f.sequence(:last_name) { |n| "bar#{n}" }
-    f.sequence(:handle){|n| "handle #{n+1}" } 
-    f.sequence(:uid){|n| n+1 } 
-    f.password "foobar"
-    f.password_confirmation { |u| u.password }
   end
 end
