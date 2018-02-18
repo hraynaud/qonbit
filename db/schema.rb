1ca6462732c92003554722bf6f8df7139d8d2b1b
hraynaud <herby.raynaud@gmail.com>
2018-02-18 18:03:01 -0500

    Adds node_id t user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213010132) do
+ActiveRecord::Schema.define(version: 20180218182502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20180213010132) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "node_id"
   end
 
   add_foreign_key "direct_auths", "users"
