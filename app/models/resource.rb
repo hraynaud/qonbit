c275f4c288a7440132694fb3dc2bcd832c6572bf
hraynaud <herby.raynaud@gmail.com>
2018-02-24 18:14:01 -0500

    writes basic resource model spec

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,8 @@
 class Resource < ApplicationRecord
   belongs_to :domain
-  belongs_to :User
+  belongs_to :user
   belongs_to :specialist, :class_name => "User", :foreign_key => "specialist_id"
 
   enum status: [ :pending, :confirmed, :rejected, :canceled ]
+
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Resource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Resource do
+    context "without user, specialist and topic" do
+      it {is_expected.to be_invalid}
+    end
+
+    context "with user, specialist and topic" do
+      subject{complete_resource}
+
+      it {is_expected.to be_valid}
+
+      it "is pending" do
+        expect(subject.pending?).to be true
+      end
+    end
+  end
+
+  private
+
+  def complete_resource
+    user = User.new
+    specialist = User.new
+    resource = Resource.new
+    domain = Domain.new
+    resource.specialist = specialist
+    resource.user = user
+    resource.domain = domain
+    resource
+  end
+
 end
