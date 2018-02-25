06944c2f64e7730fffd05b777402cbb072f42746
hraynaud <herby.raynaud@gmail.com>
2018-02-24 21:48:01 -0500

    Adds tests for auth to user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
 
   factory :user  do |f|
+    trait :with_auth_tokens do
+      after(:build) do |user, evaluator|
+        user.user_oauth_tokens.build(token: "12345", secret: "secret", provider: "oauth")
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context "with oauth tokens" do
+    subject(:user){FactoryBot.build(:user)}
+    it {is_expected.to be_invalid}
+  end
 
-  it {is_expected.to be_valid}
+  context "with oauth tokens" do
+    subject(:user){FactoryBot.build(:user, :with_auth_tokens)}
+    it {is_expected.to be_valid}
 
-  it "adds user to social graph when created" do
-    user = User.new
-    expect(user).to receive(:add_to_social_graph)
-    user.save
+    it "adds user to social graph when created" do
+      expect(user).to receive(:add_to_social_graph)
+      user.save
+    end
   end
 end
