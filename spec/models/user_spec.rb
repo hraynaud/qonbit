13a9b8df8caeddcd4ce5cb9996010b9c45a602f6
hraynaud <herby.raynaud@gmail.com>
2018-02-24 22:41:13 -0500

    adds spec to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe User, type: :model do
   context "with oauth tokens" do
     subject(:user){FactoryBot.build(:user)}
     it {is_expected.to be_invalid}
+
+    it "does not add itself to social graph " do
+      expect(user).to_not receive(:add_to_social_graph)
+      user.save
+    end
   end
 
   context "with oauth tokens" do
