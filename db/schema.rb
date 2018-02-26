d328cef838f761f81ad1cd8811013b3ab32e457e
hraynaud <herby.raynaud@gmail.com>
2018-02-25 22:14:39 -0500

    Adds user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224224351) do
+ActiveRecord::Schema.define(version: 20180226024213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 20180224224351) do
     t.string "secret", null: false
     t.string "provider"
     t.index ["token"], name: "index_oauths_on_token"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "projects", id: :serial, force: :cascade do |t|
@@ -87,6 +96,7 @@ ActiveRecord::Schema.define(version: 20180224224351) do
   end
 
   add_foreign_key "direct_auths", "users"
+  add_foreign_key "profiles", "users"
   add_foreign_key "projects", "users"
   add_foreign_key "resources", "domains"
   add_foreign_key "resources", "users"
